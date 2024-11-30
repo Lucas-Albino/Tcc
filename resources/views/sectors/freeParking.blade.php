389b21533b68325e4ff0f69c52f2eb188b53c928
Lucas Albino <lucasalbino1@yahoo.com.br>
2024-11-30 15:30:08 -0300

    alteracoes de visualizacao da tabela

--- resources/views/sectors/freeParking.blade.php
+++ resources/views/sectors/freeParking.blade.php
@@ -1,8 +1,8 @@
 @extends('main')
 
 @section('content')
-    <div class="container-xxl">
-        <div>
+    <div class="container-xxl vh-100 d-flex align-items-center justify-content-center">
+        <div class="w-100 h-100">
             <table class="table table-bordered text-center">
                 <thead>
                     <tr>
@@ -10,15 +10,15 @@
                         <th>Status</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody class="text-white">
                     @forelse ($freeParking as $vaga)
                         <tr class={{ $vaga['_value'] ? 'bg-success' : 'bg-danger' }}>
-                            <td>{{ $vaga['sensor'] }}</td>
-                            <td>{{ $vaga['_value'] ? 'Disponível' : 'Indisponível' }}</td>
+                            <td><b>{{ $vaga['sensor'] }}</b></td>
+                            <td><b>{{ $vaga['_value'] ? 'Disponível' : 'Indisponível' }}</b></td>
                         </tr>
                     @empty
-                        <tr>
-                            <td colspan="2">Setor indisponivel no momento</td>
+                        <tr class="text-dark">
+                            <td colspan="2" class="text-stroke"><b>Nenhuma vaga disponivel no momento</b></td>
                         </tr>
                     @endforelse
                 </tbody>
--- resources/views/sectors/sector1.blade.php
+++ resources/views/sectors/sector1.blade.php
@@ -1,8 +1,8 @@
 @extends('main')
 
 @section('content')
-    <div class="container-xxl">
-        <div>
+    <div class="container-xxl vh-100 d-flex align-items-center justify-content-center">
+        <div class="w-100 h-100">
             <table class="table table-bordered text-center">
                 <thead>
                     <tr>
@@ -10,15 +10,15 @@
                         <th>Status</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody class="text-white">
                     @forelse ($setor1 as $vaga)
                         <tr class={{ $vaga['_value'] ? 'bg-success' : 'bg-danger' }}>
-                            <td>{{ $vaga['sensor'] }}</td>
-                            <td>{{ $vaga['_value'] ? 'Disponível' : 'Indisponível' }}</td>
+                            <td><span class="text-stroke"><b>{{ $vaga['sensor'] }}</b></span></td>
+                            <td><span class="text-stroke"><b>{{ $vaga['_value'] ? 'Disponível' : 'Indisponível' }}</b></span></td>
                         </tr>
                     @empty
-                        <tr>
-                            <td colspan="2">Setor indisponivel no momento</td>
+                        <tr class="text-dark">
+                            <td colspan="2" class="text-stroke">Setor indisponível no momento</td>
                         </tr>
                     @endforelse
                 </tbody>
